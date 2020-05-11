86fc36907a53eaf5fe2f58bd27f4d791c11efb39
yetno <47375229+yetno@users.noreply.github.com>
2020-05-11 11:25:32 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -5,6 +5,6 @@ export default [
     { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "Keep Fighting and Ganbatte" },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
 ];
