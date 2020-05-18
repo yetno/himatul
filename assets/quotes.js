46587edffb9f0f1e0c06ff570ef3b7a093ddf183
yetno <47375229+yetno@users.noreply.github.com>
2020-05-18 21:32:46 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -3,7 +3,7 @@ export default [
     { author: "yit", quotes: "Whatever happens, keep breathing" },
     { author: "yit", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "yit", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "yit", quotes: "I love you." },
+    { author: "yit", quotes: "I'm sorry for all." },
     { author: "yit", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "yit", quotes: "Keep Fighting and Ganbatte" },
     { author: "yit", quotes: "I'm sorry for loving you." },
